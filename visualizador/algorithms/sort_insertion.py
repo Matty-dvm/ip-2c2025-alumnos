d0690e1a14edf685d6a43a64314a89496093b66d
Matty-dvm <matiasmacielk@gmail.com>
2025-11-24 20:23:52 -0300

    Se agrego desarrollaron las funciones en sort_insertion.py

--- visualizador/algorithms/sort_insertion.py
+++ visualizador/algorithms/sort_insertion.py
@@ -1,21 +1,38 @@
-# Contrato: init(vals), step() -> {"a": int, "b": int, "swap": bool, "done": bool}
-
 items = []
 n = 0
-i = 0      # elemento que queremos insertar
-j = None   # cursor de desplazamiento hacia la izquierda (None = empezar)
+i = 1
+j = 0
 
 def init(vals):
     global items, n, i, j
     items = list(vals)
     n = len(items)
-    i = 1      # común: arrancar en el segundo elemento
-    j = None
+    i = 1
+    j = i - 1
 
 def step():
-    # TODO:
-    # - Si i >= n: devolver {"done": True}.
-    # - Si j es None: empezar desplazamiento para el items[i] (p.ej., j = i) y devolver un highlight sin swap.
-    # - Mientras j > 0 y items[j-1] > items[j]: hacer UN swap adyacente (j-1, j) y devolverlo con swap=True.
-    # - Si ya no hay que desplazar: avanzar i y setear j=None.
-    return {"done": True}
+    global items, n, i, j
+
+    if n <= 1 or i >= n:
+        return {"done": True}
+
+    a = j
+    b = j + 1
+    swap = False
+
+    if items[b] < items[a]:
+        items[a], items[b] = items[b], items[a]
+        swap = True
+        j -= 1
+
+        if j < 0:
+            i += 1
+            j = i - 1
+
+        return {"a": a, "b": b, "swap": swap, "done": False}
+
+
+    i += 1
+    j = i - 1
+
+    return {"a": a, "b": b, "swap": False, "done": False}
